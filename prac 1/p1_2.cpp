ad66472d537c5462bae8b28418fd3b352a23a7fa
25dit017-codes <25dit017@charusat.edu.in>
2026-08-02 22:44:18 +0530

    Update p1_2.cpp

--- prac 1/p1_2.cpp
+++ prac 1/p1_2.cpp
@@ -1,41 +1,37 @@
-#include<bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int main()
-{
+int main() {
     int n;
+
+    cout << "Enter number of borrow records: ";
     cin >> n;
 
-    int arr[n];
+    int books[n];
 
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    for(int i = 0; i < n; i++)
-    {
-       
-        bool printed = false;
-        for(int k = 0; k < i; k++)
-        {
-            if(arr[i] == arr[k])
-            {
-                printed = true;
-                break;
-            }
-        }
-
-        if(printed) continue;
-
-    
-        for(int j = i + 1; j < n; j++)
-        {
-            if(arr[i] == arr[j])
-            {
-                cout << arr[i] << " ";
-                break;
-            }
-        }
+    cout << "Enter Book IDs:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> books[i];
     }
-         
+
+    cout << "Book IDs borrowed more than once:\n";
+    for (int i = 0; i < n; i++) {
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            if (books[i] == books[j])
+                count++;
+        }
+
+        int printed = 0;
+        for (int k = 0; k < i; k++) {
+            if (books[i] == books[k])
+                printed = 1;
+        }
+
+        if (count > 1 && printed != 1){
+            cout << books[i] << " ";}
+    }
+
     return 0;
 }
